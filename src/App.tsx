12e2536b3f25277dbff527a6a7c31080bb98ce01
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2024-10-02 16:56:19 +0200

    ajout style page

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,9 @@ import ProjectList from './composants/ProjectList';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
       <div>
-        <Header />
         <ProjectList />
-        {/* Ajoute d'autres sections comme À propos et Contact ici */}
       </div>
     </div>
 
